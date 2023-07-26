cb58a0b51500fa9cd14b6224b6358451aee0b46b
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-07-26 14:20:52 -0300

    add display function

--- src/index.js
+++ src/index.js
@@ -1,35 +1,47 @@
 import './style.css';
 
-// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-//   method: 'POST',
-//   body: JSON.stringify({
-//     name: 'my new game',
-//   }),
-//   headers: {
-//     'Content-type': 'application/json; charset=UTF-8',
-//   },
-// })
-// // Exito
-//   .then((response) => response.json()) // convertir a json
-//   .then((json) => console.log(json)) // imprimir los datos en la consola
-//   .catch((err) => console.log('Solicitud fallida', err)); // Capturar errores
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+  method: 'POST',
+  body: JSON.stringify({
+    name: 'my new game',
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+// Exito
+  .then((response) => response.json()) // convertir a json
+  .then((json) => console.log(json)) // imprimir los datos en la consola
+  .catch((err) => console.log('Solicitud fallida', err)); // Capturar errores
 
 const scoreList = document.querySelector('#gameScoreList');
 const fName = document.querySelector('#fName');
 const fScore = document.querySelector('#fScore');
 const fButton = document.querySelector('#fButton');
 
+const clearList = () => {
+  while (scoreList.firstChild) {
+    scoreList.removeChild(scoreList.firstChild);
+  }
+};
+
 const display = (arr) => {
   arr.result.forEach((element) => console.log(element));
+  clearList();
+  for (let i = 0; i < arr.result.length; i++) {
+    const newItem = document.createElement('div');
+    newItem.textContent = arr.result[i].user + arr.result[i].score;
+    scoreList.appendChild(newItem);
+  }
 };
 const refreshScores = () => {
-  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/37eiexTkKSur5UWYeeYT/scores/')
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/eTp1wZzwkSLFf86ks2kW /scores/')
     .then((response) => response.json())
     .then((json) => display(json));
 };
 
 const submitScore = () => {
-  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/37eiexTkKSur5UWYeeYT/scores/', {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/eTp1wZzwkSLFf86ks2kW /scores/', {
     method: 'POST',
     body: JSON.stringify({
       user: fName.value,
@@ -41,7 +53,6 @@ const submitScore = () => {
       'Content-type': 'application/json; charset=UTF-8',
     },
   })
-    .then((response) => response.json())
     .then(() => refreshScores());
 };
 fButton.addEventListener('click', submitScore);
