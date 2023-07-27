cc94af15f97c9af54ce88aa3263d2eb9f2821979
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-07-27 16:45:18 -0300

    add buttons styles and list animation

--- src/modules/usingAPI.js
+++ src/modules/usingAPI.js
@@ -1,9 +1,6 @@
-// const scoreList = document.querySelector('#gameScoreList');
+const scoreList = document.querySelector('#gameScoreList');
 const fName = document.querySelector('#fName');
 const fScore = document.querySelector('#fScore');
-const scoreListHolder = document.querySelector('#scoreListHolder');
-const scoreList = document.createElement('ul');
-scoreList.classList.add('scoreList');
 const myId = 'eTp1wZzwkSLFf86ks2kWD';
 // eslint-disable-next-line prefer-template
 const myUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/' + myId + '/scores/';
@@ -14,7 +11,6 @@ const clearList = () => {
 };
 const display = (arr) => {
   clearList();
-  scoreListHolder.removeChild(scoreListHolder.firstChild);
   const newArr = arr.result;
   newArr.forEach((elem) => {
     elem.score = parseInt(elem.score, 10);
@@ -23,24 +19,23 @@ const display = (arr) => {
   sortedArr.forEach((elem) => {
     const newItem = document.createElement('li');
     newItem.textContent = `${elem.user} : ${elem.score}`;
-    scoreList.appendChild(newItem);
     const i = newArr.indexOf(elem);
     newItem.classList.add('scoreItem');
     switch (i) {
       case 0:
-        newItem.classList.add('firstPlace');
+        newItem.classList.add('firstPlace', 'mobileAnimation');
         break;
       case 1:
-        newItem.classList.add('secondPlace');
+        newItem.classList.add('secondPlace', 'mobileAnimation');
         break;
       case 2:
-        newItem.classList.add('thirdPlace');
+        newItem.classList.add('thirdPlace', 'mobileAnimation');
         break;
       default:
         newItem.classList.add('whiteItem');
     }
+    scoreList.appendChild(newItem);
   });
-  scoreListHolder.appendChild(scoreList);
 };
 
 const refreshScores = async () => {
